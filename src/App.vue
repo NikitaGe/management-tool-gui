6c937c3bd1aea85a706fe81b73ced828ac121839
Nikita Gerasimchuk <nikitage2002@gmail.com>
2024-11-12 20:24:16 +0100

    config

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -13,7 +13,7 @@ import { RouterLink, RouterView } from 'vue-router'
       </nav>
     </div>
   </header>
-
   <RouterView />
+
 </template>
 
